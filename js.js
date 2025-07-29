a390fca9884f6c9ac53f32e3bcde83caac077b93
alyx-ba <dsai2411003009@gmail.com>
2025-07-28 22:16:32 -0700

    Add files via upload

---
+++ js.js
@@ -0,0 +1,52 @@
+// let num = 2;
+// for(let num=2; num<=100; num++){
+//     let isprime=true;
+//     for(let i=5; i<=Math.sqrt(num); i++){
+//         if(num% i===0){
+//             isprime=false;
+//             break;
+//         }
+//     }
+//     if(isprime){
+//         console.log(num);
+//     }
+// }
+
+// for loop
+let num=prompt("Enter a number");
+let i=1;
+for(let i= 1; i<=10; i++){
+console.log(`${num}x ${i} = ${num*i}`);
+  }
+//   while loop
+let number=prompt("Enter a even number");
+let a =1;
+while(a<=10){
+   console.log(`${number} x ${a}= ${number*i}`);
+   a++;
+}
+// do while
+
+ var ah =prompt("Enter a integer");
+ let b= 1;
+ do {
+   console.log(`${ah} x ${b}= ${ah*b}`);
+   b++;
+ } while(b <= 10);
+// function Isevenodd(a){
+// return a;
+// }
+// let a = 12;  
+// if(a%2===0){
+//     console.log("num is even");
+//  }else{
+//     console.log("num is odd");
+
+//     }
+   //  let colour = ["purple","orange","blue","lightslatery","gray","green","pink","black","red","lightgreen","aqua"]
+  
+   // for(let i=0; i<=10; i++){
+   //    console.log(colour[i]);
+   // }
+ 
+
